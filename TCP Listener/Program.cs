f13ee4b57212e47b88b588a229d4b88a155374a0
Talib_ip88 <Talib_ip88@STHQ012B-07>
2024-09-27 19:06:44 +0400

    Add project files.

---
+++ TCP Listener/Program.cs
@@ -0,0 +1,98 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+
+TcpListener listener = null;
+BinaryReader br = null;
+BinaryWriter bw = null;
+List<TcpClient> clients = [];
+
+var ip = IPAddress.Parse("127.0.0.1");
+var port = 27001;
+var endPoint = new IPEndPoint(ip, port);
+listener = new TcpListener(endPoint);
+listener.Start(2);
+Console.WriteLine($"Listening on {listener.LocalEndpoint}");
+
+
+string[][] TicTacToe = [];
+
+while (true)
+{
+    var client = listener.AcceptTcpClient();
+    clients.Add(client);
+    Console.WriteLine($"{client.Client.RemoteEndPoint} connected...");
+
+    var reader = Task.Run(() =>
+    {
+        foreach (var client in clients)
+        {
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    var stream = client.GetStream();
+                    br = new BinaryReader(stream);
+                    bw = new BinaryWriter(stream);
+                    try
+                    {
+                        var msg = br.ReadString();
+                        Console.WriteLine($"Client {client.Client.RemoteEndPoint}: {msg[0]}");
+                        Console.WriteLine($"Client {client.Client.RemoteEndPoint}: {msg[1]}");
+                        Console.WriteLine($"Client {client.Client.RemoteEndPoint}: {msg[2]}");
+                        var row = int.Parse(msg[1].ToString());
+                        var column = int.Parse(msg[2].ToString());
+                        TicTacToe[row][column] = msg[0].ToString();
+                        
+                        var winner = CheckWinner(bw);
+                        if (winner != string.Empty)
+                        {
+                            bw.Write(winner);
+                        }
+                        bw.Write(msg[0]);              
+                    }
+                    catch (Exception)
+                    {
+                        clients.Remove(client);
+                    }
+                }
+            }).Wait(50);
+        }
+    });
+}
+string CheckWinner(BinaryWriter bw)
+{
+    if (TicTacToe[0][0] == TicTacToe[0][1] && TicTacToe[0][0] == TicTacToe[0][2])
+    {
+        return TicTacToe[0][0] ;
+    }
+    else if (TicTacToe[0][0] == TicTacToe[1][0] && TicTacToe[0][0] == TicTacToe[2][0])
+    {
+        return (TicTacToe[0][0]);
+    }
+    else if (TicTacToe[0][0] == TicTacToe[1][1] && TicTacToe[0][0] == TicTacToe[2][2])
+    {
+        return (TicTacToe[0][0]);
+    }
+    else if (TicTacToe[1][0] == TicTacToe[1][1] && TicTacToe[1][0] == TicTacToe[1][2])
+    {
+        return (TicTacToe[1][0]);
+    }
+    else if (TicTacToe[2][0] == TicTacToe[2][1] && TicTacToe[2][0] == TicTacToe[2][2])
+    {
+        return (TicTacToe[1][0]);
+    }
+    else if (TicTacToe[0][1] == TicTacToe[1][1] && TicTacToe[0][1] == TicTacToe[2][1])
+    {
+        return (TicTacToe[0][1]);
+    }
+    else if (TicTacToe[0][2] == TicTacToe[1][2] && TicTacToe[0][2] == TicTacToe[2][2])
+    {
+        return (TicTacToe[0][2]);
+    }
+    else if (TicTacToe[0][2] == TicTacToe[1][1] && TicTacToe[0][2] == TicTacToe[2][1])
+    {
+        return (TicTacToe[0][2]);
+    }
+    return string.Empty;
+}
---
+++ Tic Tac Toe Client/MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.IO;
+using System.Windows.Threading;
+
+namespace Tic_Tac_Toe_Client;
+
+public partial class MainWindow : Window
+{
+    TcpClient? client = new TcpClient();
+
+    IPAddress? ip = null;
+    int port = 0;
+    IPEndPoint? endPoint = null;
+    public bool Turn { get; set; } = true;
+
+    Dispatcher Dispatcher { get; set; }
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        ip = IPAddress.Parse("127.0.0.1");
+        port = 27001;
+        endPoint = new IPEndPoint(ip, port);
+        Dispatcher = Dispatcher.CurrentDispatcher;
+        client.Connect(endPoint);
+    }
+
+    private void btn_Click(object sender, RoutedEventArgs e)
+    {
+        var btn = sender as Button;
+
+        var stream = client.GetStream();
+        var bw = new BinaryWriter(stream);
+        var br = new BinaryReader(stream);
+        var text = "";
+        var row = "";
+        var column = "";
+        if (btn == btn1 || btn == btn2 || btn == btn3)
+        {
+            row = "0";
+        }
+        else if (btn == btn4 || btn == btn5 || btn == btn6)
+        {
+            row = "1";
+        }
+        else if (btn == btn7 || btn == btn8 || btn == btn9)
+        {
+            row = "2";
+        }
+
+        if (btn == btn1 || btn == btn4 || btn == btn7)
+        {
+            column = "0";
+        }
+        else if (btn == btn2 || btn == btn5 || btn == btn8)
+        {
+            column = "1";
+        }
+        else if (btn == btn3 || btn == btn6 || btn == btn9)
+        {
+            column = "2";
+        }
+
+        if (btn is not null)
+        {
+            try
+            {
+                if (Turn)
+                {
+
+                    if (client.Connected)
+                    {
+
+                        text = "X";
+                        var str = text + " " + row + " " + column;
+                        bw.Write(str);
+
+                        btn.Content = br.ReadString();
+
+                    }
+                }
+                else
+                {
+                    if (client.Connected)
+                    {
+                        var writer = Task.Run(() =>
+                        {
+
+                            text = "O";
+                            bw.Write(text + " " + row + " " + column);
+
+                        });
+
+
+                        if (br.ReadString() == "X" || br.ReadString() == "O")
+                            btn.Content = br.ReadString();
+                        else
+                            MessageBox.Show($"Congrats {br.ReadString()} you are the winner");
+
+
+
+                    }
+                }
+                Turn = !Turn;
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
